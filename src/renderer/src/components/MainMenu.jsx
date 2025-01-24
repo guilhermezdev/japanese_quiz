ea2be4694e1775018610391ac48a7229b14aaba6
Guilherme Zamberlam Pomini <guizamberlam@hotmail.com>
2025-01-23 21:40:59 -0300

    updating selection on menu

--- src/renderer/src/components/MainMenu.jsx
+++ src/renderer/src/components/MainMenu.jsx
@@ -1,8 +1,10 @@
 import Card from 'react-bootstrap/Card'
 import ListGroup from 'react-bootstrap/ListGroup'
-import { hiraganaList } from './japanese_characters'
+import { hiraganaList, katakanaList } from './japanese_characters'
+import { useState } from 'react'
+
 function MainMenu() {
-  const groupedKana = hiraganaList.reduce((acc, kana) => {
+  const groupByCategory = (kanaList) => kanaList.reduce((acc, kana) => {
     if (!acc[kana.category]) {
       acc[kana.category] = []
     }
@@ -10,22 +12,40 @@ function MainMenu() {
     return acc
   }, {})
 
+  const groupedHiragana = groupByCategory(hiraganaList)
+  const groupedKatakana = groupByCategory(katakanaList)
+
+  const [selected, setSelected] = useState([])
+
+  const changeSelection = (value) => {
+    if(selected.includes(value)){
+      setSelected(selected.filter((item) => item !== value))
+    }else{
+        setSelected([...selected, value])
+    }
+  }
+
+
   return (
-    <div>
+    <div style={{
+        margin: '16px'
+    }}>
       <Card>
         <Card.Title>Japanese Quiz</Card.Title>
         <Card.Body>Train your Hiragana and Katakana knowledge.</Card.Body>
       </Card>
+
+      <br></br>
 
       <Card style={{ width: '20rem' }}>
         <Card.Header>
           <Card.Title>Hiragana - ひらがな</Card.Title>
         </Card.Header>
         <ListGroup variant="flush">
-          {Object.keys(groupedKana).map((group) => (
-            <ListGroup.Item action key={group[0].category}>
+          {Object.keys(groupedHiragana).map((group) => (
+            <ListGroup.Item  action key={group[0].category} onClick={() => changeSelection('hiragana-' + group)} active={selected.includes('hiragana-' + group)}>
               {group} -{' '}
-              {groupedKana[group]
+              {groupedHiragana[group]
                 .slice(0, 5)
                 .map((kana) => kana.character)
                 .join(' ')}
